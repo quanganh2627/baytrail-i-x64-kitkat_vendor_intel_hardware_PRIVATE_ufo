a7ebb5aa00949020f7af14e62ca59775c7f2c067
Dale Stimson <dale.b.stimson@intel.com>
2013-04-08 11:16:15 -0700

    BYT: ufo binary release based on ufo CL 114769 plus changes.
    BZ: 97395
    
    Binary build based on ufo CL 114769 plus changes.
    
    Change-Id: I5564bc7c8474cdbb4457eeb9ee8612c8e29d9568
    Signed-off-by: Dale Stimson <dale.b.stimson@intel.com>

--- Android.mk
+++ Android.mk
@@ -2,6 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 
 ifneq ($(BOARD_HAVE_GEN_GFX_SRC),true)
 
+UFO_PROJECT_PATH := $(LOCAL_PATH)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := ufo
 LOCAL_MODULE_TAGS := optional
@@ -13,7 +15,7 @@ $(LOCAL_INSTALLED_MODULE): $(LOCAL_MODULE)_always_extract
 .PHONY: $(LOCAL_MODULE)_always_extract
 $(LOCAL_MODULE)_always_extract: PRIVATE_PATH := $(LOCAL_PATH)
 $(LOCAL_MODULE)_always_extract:
-	$(hide) echo "GEN GFX: $@"
+	$(hide) echo "$(UFO_PROJECT_PATH): GEN GFX: $@"
 	$(hide) mkdir -p $(dir $@)
 	$(hide) $(PRIVATE_PATH)/dist_install.sh $(PRIVATE_PATH)/dist $(PRODUCT_OUT)
 
--- dist_install.sh
+++ dist_install.sh
@@ -16,12 +16,105 @@ set -u
 proc_path="${BASH_SOURCE[0]}"
 pnm="${proc_path##*/}"
 
-opt_verbose=1
+opt_help=0
 opt_dry_run=0
+opt_diff=0
+opt_rm=0
+dir_from=""
+dir_to=""
+opt_verbose=1
 
-if [ -n "${1:-}" ] ; then
-    dir_from="${1}"
-else
+param_list=( "${@:+${@}}" )
+
+errdet=0
+nparams=0
+for param in "${param_list[@]:+${param_list[@]}}" ; do
+    kw_arg="${param#*=}"
+    if [ "${param}" == "${kw_arg}" ] ; then
+	kw_arg=""
+    fi
+    case "${param}" in
+        -h|--help)
+            opt_help=1
+            ;;
+        --diff)
+            opt_diff=1
+            ;;
+        --no-diff)
+            opt_diff=0
+            ;;
+        --rm)
+            opt_rm=1
+            ;;
+        --no-rm)
+            opt_rm=0
+            ;;
+        --dry-run)
+            opt_dry_run=1
+            ;;
+        --no-dry-run)
+            opt_dry_run=0
+            ;;
+        -q)
+            opt_verbose=$((opt_verbose-1))
+            ;;
+        -v)
+            opt_verbose=$((opt_verbose+1))
+            ;;
+        -*)
+            echo >&2 "${pnm}: Error: Unrecognized option: ${param}"
+            errdet=1
+            ;;
+        *)
+            case "${nparams}" in
+                0)
+                    dir_from="${param}"
+                    ;;
+                1)
+                    dir_to="${param}"
+                    ;;
+                *)
+                    echo >&2 "${pnm}: Error: Unexpected parameter: ${param}"
+                    errdet=1
+                    ;;
+            esac
+            nparams=$((nparams+1))
+            ;;
+    esac
+done
+
+if [ ${opt_help} -ne 0 ] ; then
+    cat <<EOF
+Install ufo distribution files under \${ANDROID_PRODUCT_OUT}.
+
+usage: $0 [options] [dir_from] [dir_to]
+
+dir_from is the directory containing the ufo library binary distribution,
+which is usually ./dist, relative to the ufo binary repostory.
+
+dir_to is usually \${ANDROID_PRODUCT_OUT}.
+
+Options are:
+-h             Output this help text.
+--help         Output this help text.
+--diff         Compare files.  Do not transfer them.
+--no-diff      Transfer the files.
+--rm           Remove target files.
+--no-rm        Do not remove target files.
+--dry-run      Avoid copying or changing any files
+--no-dry-run
+-q             Be less verbose
+-v             Be more verbose.
+EOF
+    exit
+fi
+
+if [ ${errdet} -ne 0 ] ; then
+    echo >&2 "${pnm}: Aborting due to previously detected error(s)"
+    exit 1
+fi
+
+if [ -z "${dir_from}" ] ; then
     dir_from=$( dirname "${proc_path}" )
     if [ "${dir_from}" == "." ] ; then
         dir_from="dist"
@@ -30,14 +123,12 @@ else
     fi
 fi
 
-if [ -n "${2:-}" ] ; then
-    dir_to="${2}"
-else
+if [ -z "${dir_to}" ] ; then
     dir_to=${ANDROID_PRODUCT_OUT}
 fi
 
 if [ ! -d "${dir_from}" ] ; then
-    echo >&2 "${pnm}: Error: Required directory not present: "${dir_from}""
+    echo >&2 "${pnm}: Error: Required directory not present: \"${dir_from}\""
     exit 1
 fi
 
@@ -94,35 +185,73 @@ for dist_file in "${dist_list[@]:+${dist_list[@]}}" ; do
     # This will help to avoid needless rebuilds by users of the libraries.
 
     # xzdiff recognizes compression types none, xz, lzma, gzip, bzip2, and lzop.
-    if [ -e "${path_to}" ] && ${pgm_diff} -q "${path_from}" "${path_to}" >/dev/null ; then
+    if [ ! -e "${path_to}" ] ; then
+        files_are_identical=missing_to
+    elif ${pgm_diff} -q "${path_from}" "${path_to}" >/dev/null ; then
         # Files are the same.  Take no action.
-        if [ ${opt_verbose} -ge 1 ] ; then
-            echo >&2 "${pnm}: Info: File is unchanged: ${path_from}"
+        files_are_identical=y
+    else
+        files_are_identical=n
+    fi
+
+    if [ ${opt_diff} -ne 0 ] ; then
+        if [ "${files_are_identical}" == "y" ] ; then
+            if [ ${opt_verbose} -ge 1 ] ; then
+                echo >&2 "${pnm}: Info: File  same:   ${path_from} ${path_to}"
+            fi
+        elif [ "${files_are_identical}" == "n" ] ; then
+            echo >&2 "${pnm}: Info: Files differ: ${path_from} ${path_to}"
+        elif [ "${files_are_identical}" == "missing_to" ] ; then
+            echo >&2 "${pnm}: Info: File not present:  ${path_from}"
+        else
+            echo >&2 "${pnm}: Info: unknown diff issue: ${files_are_identical} : ${path_from} ${path_to}"
+        fi
+    elif [ ${opt_rm} -ne 0 ] ; then
+        if [ ! -e "${path_to}" ] ; then
+            echo >&2 "${pnm}: Info: File already absent: ${path_to}"
+        else
+            echo >&2 "${pnm}: Info: Removing: ${path_to}"
+            if [ ${opt_verbose} -ge 1 ] || [ ${opt_dry_run} -ne 0 ] ; then
+                echo >&2 "+ " rm "${path_to}"
+            fi
+            if [ ${opt_dry_run} -eq 0 ] ; then
+                rm "${path_to}"
+            fi
         fi
     else
-        path_to_dir=$( dirname "${dir_to}/${dist_file}" )
-        if [ ! -d "${path_to_dir}" ] ; then
-            if [ ${opt_verbose} -ge 1 ] || [ ${opt_dry_run} -ne 0 ] ; then
-                echo >&2 "+ " mkdir -p "${path_to_dir}"
-            fi
-            if [ ${opt_dry_run} -eq 0 ] ; then
-                mkdir -p "${path_to_dir}"
-            fi
-        fi
-
-        if [ ${flag_redirect} -eq 0 ] ; then
-            if [ ${opt_verbose} -ge 1 ] || [ ${opt_dry_run} -ne 0 ] ; then
-                echo >&2 "+ " ${cpcmd} "${path_from}" "${path_to}"
-            fi
-            if [ ${opt_dry_run} -eq 0 ] ; then
-                ${cpcmd} "${path_from}" "${path_to}"
+        if [ "${files_are_identical}" == "y" ] ; then
+            if [ ${opt_verbose} -ge 1 ] ; then
+                echo >&2 "${pnm}: Info: File is unchanged: ${path_from}"
             fi
         else
-            if [ ${opt_verbose} -ge 1 ] || [ ${opt_dry_run} -ne 0 ] ; then
-                echo >&2 "+ " ${cpcmd} "${path_from}" ">${path_to}"
+            if [ "${files_are_identical}" != "n" ] && [ "${files_are_identical}" != "missing_to" ] ; then
+                echo >&2 "${pnm}: Info: unknown diff problem: ${files_are_identical} : ${path_from} ${path_to}"
             fi
-            if [ ${opt_dry_run} -eq 0 ] ; then
-                ${cpcmd} "${path_from}" >"${path_to}"
+
+            path_to_dir=$( dirname "${dir_to}/${dist_file}" )
+            if [ ! -d "${path_to_dir}" ] ; then
+                if [ ${opt_verbose} -ge 1 ] || [ ${opt_dry_run} -ne 0 ] ; then
+                    echo >&2 "+ " mkdir -p "${path_to_dir}"
+                fi
+                if [ ${opt_dry_run} -eq 0 ] ; then
+                    mkdir -p "${path_to_dir}"
+                fi
+            fi
+
+            if [ ${flag_redirect} -eq 0 ] ; then
+                if [ ${opt_verbose} -ge 1 ] || [ ${opt_dry_run} -ne 0 ] ; then
+                    echo >&2 "+ " ${cpcmd} "${path_from}" "${path_to}"
+                fi
+                if [ ${opt_dry_run} -eq 0 ] ; then
+                    ${cpcmd} "${path_from}" "${path_to}"
+                fi
+            else
+                if [ ${opt_verbose} -ge 1 ] || [ ${opt_dry_run} -ne 0 ] ; then
+                    echo >&2 "+ " ${cpcmd} "${path_from}" ">${path_to}"
+                fi
+                if [ ${opt_dry_run} -eq 0 ] ; then
+                    ${cpcmd} "${path_from}" >"${path_to}"
+                fi
             fi
         fi
     fi
