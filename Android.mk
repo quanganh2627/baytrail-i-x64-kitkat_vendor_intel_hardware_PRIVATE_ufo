88899704a8c65e24ef3c60f8f15f7217ed7350b0
Dale Stimson <dale.b.stimson@intel.com>
2013-05-15 13:07:54 -0700

    BYT: ufo binary - Create libastl.a, needed for ufo source build
    BZ: 108754
    
    File libastl.a (from project "a/aosp/platform/external/astl" directory
    external/astl) is a dependency for the ufo source build.  For the
    convenience of people working from source and using "mm" (which does
    not build dependencies) add libastl.a as a dependency for this project.
    
    Change-Id: Ib0394fbdce0a8394da384dd7f6ceda3c130824b7
    Signed-off-by: Dale Stimson <dale.b.stimson@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,21 +1,46 @@
-LOCAL_PATH:= $(call my-dir)
-
 ifneq ($(BOARD_HAVE_GEN_GFX_SRC),true)
+
+LOCAL_PATH:= $(call my-dir)
 
 UFO_PROJECT_PATH := $(LOCAL_PATH)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := ufo
 LOCAL_MODULE_TAGS := optional
-include $(BUILD_PHONY_PACKAGE)
 
-# Always extract the tarball through a phony target
+# libastl - From external/asti, required for building these packages.
+LOCAL_STATIC_LIBRARIES := libastl
+
+# Replace $(BUILD_PHONY_PACKAGE) with a kludge that will generate
+# pre-requisites for the package, particularly $(LOCAL_STATIC_LIBRARIES).
+# Note: Variable "all_libraries" is a build system internal variable.
+
+## include $(BUILD_PHONY_PACKAGE)
+
+# ------------------------------------------------------------------------
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+
+include $(BUILD_SYSTEM)/binary.mk
+
+$(LOCAL_BUILT_MODULE): $(all_libraries)
+
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Fake: $@"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) touch $@
+
+# ------------------------------------------------------------------------
+
 $(LOCAL_INSTALLED_MODULE): $(LOCAL_MODULE)_always_extract
 
 .PHONY: $(LOCAL_MODULE)_always_extract
+
 $(LOCAL_MODULE)_always_extract: PRIVATE_PATH := $(LOCAL_PATH)
+
 $(LOCAL_MODULE)_always_extract:
-	$(hide) echo "$(UFO_PROJECT_PATH): GEN GFX: $@"
+	@ echo "$(UFO_PROJECT_PATH): GEN GFX: $@"
 	$(hide) mkdir -p $(dir $@)
 	$(hide) $(PRIVATE_PATH)/dist_install.sh $(PRIVATE_PATH)/dist $(PRODUCT_OUT)
 
