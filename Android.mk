8b6d0f6925ff89c4d257dc9b724c9f0442f6a35d
Dale Stimson <dale.b.stimson@intel.com>
2013-09-19 17:55:07 -0700

    BYT: Install ufo exported header files in the same way as ufosrc
    BZ: 140045
    
    Use semi-deprecated but very commonly used Android build system
    variables LOCAL_COPY_HEADERS and LOCAL_COPY_HEADERS_TO to install
    header files in the same way as is done by ufosrc.  This allows other
    modules to find the files in a common place.
    
    This change is in response to a developer request.  It affects only the
    build.
    
    The ufosrc project exports several header files for use by other
    projects using Android build system variables LOCAL_COPY_HEADERS and
    LOCAL_COPY_HEADERS_TO.  This results in the header files being copied
    to the target/product output directory (not to the target itself)
    under directory obj/include.
    
    Conversely, the ufo pre-built binary project merely included the header
    files under an "include" directory from which they could be referenced.
    
    This results in a build-time difference between using the pre-built
    binaries versus building with ufo source in the tree.  This commit
    removes that difference so that the header files are available in a
    consistent way.
    
    Variable LOCAL_COPY_HEADERS contains the name of each header file to
    be copied out, and LOCAL_COPY_HEADERS_TO contains the directory name
    into which the files are to be copied.
    
    Change-Id: I1a77f045c9439013f7d7f8847b93282397551d63
    Signed-off-by: Dale Stimson <dale.b.stimson@intel.com>

--- Android.mk
+++ Android.mk
@@ -67,17 +67,51 @@ LOCAL_EXPORT_C_INCLUDE_DIRS += $(UFO_PROJECT_PATH)/include
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 
+# Expected: TARGET_OUT_INTERMEDIATE_LIBRARIES = ${ANDROID_PRODUCT_OUT}/obj/lib
 OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
 
 # Do not strip, as debug section creation will fail because already stripped.
 LOCAL_STRIP_MODULE := false
 
+# Use of LOCAL_COPY_HEADERS is deprecated.
+
+LOCAL_COPY_HEADERS_TO := libpavp/
+
+LOCAL_COPY_HEADERS :=
+LOCAL_COPY_HEADERS += include/libpavp.h
+# How it should have been done?
+## LOCAL_COPY_HEADERS += include/libpavp/libpavp.h
+
 include $(BUILD_SYSTEM)/dynamic_binary.mk
 
-$(linked_module): $(UFO_PROJECT_PATH)/dist/system/lib/libpavp.so.xz
+# Inclusion of all_copied_headers invokes code for LOCAL_COPY_HEADERS.
+
+$(linked_module): $(UFO_PROJECT_PATH)/dist/system/lib/libpavp.so.xz all_copied_headers
 	mkdir -p $(dir $@)
 	xz -d -c $< >$@
 	chmod --reference=$< $@
+
+# ------------------------------------------------------------------------
+
+include $(CLEAR_VARS)
+
+# Use of LOCAL_COPY_HEADERS is deprecated.
+
+# Inclusion of all_copied_headers invokes code for LOCAL_COPY_HEADERS.
+# This "module" does not have a dependency for which all_copied_headers
+# can be made a prerequisite, so depend on the reference above for
+# proper execution with "mm" command.
+
+LOCAL_COPY_HEADERS_TO := ufo/
+
+LOCAL_COPY_HEADERS :=
+LOCAL_COPY_HEADERS += include/graphics.h
+LOCAL_COPY_HEADERS += include/gralloc.h
+# How it should have been done?
+## LOCAL_COPY_HEADERS += include/ufo/graphics.h
+## LOCAL_COPY_HEADERS += include/ufo/gralloc.h
+
+include $(BUILD_COPY_HEADERS)
 
 # ------------------------------------------------------------------------
 
