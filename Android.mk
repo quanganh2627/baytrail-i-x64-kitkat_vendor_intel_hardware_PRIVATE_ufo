d2c5fd496a712568cfa0555fbf9dd587054cd3bd
Yunwei Zhang <yunwei.zhang@intel.com>
2014-04-11 13:52:55 -0700

    Include ufo_headers.mk in Android.mk
    BZ: 186562
    
    cm_* headers are missing in $(OUT), this patch put ufo_headers.mk
    in the inclusion of Android.mk to make sure they are exported. It
    also removes the duplicate exports in Android.mk.
    
    Change-Id: Ice6b24cb9b0e9b6a81de516b9c6e6865196cbbfd
    Signed-off-by: Yunwei Zhang <yunwei.zhang@intel.com>

--- Android.mk
+++ Android.mk
@@ -4,6 +4,11 @@ LOCAL_PATH:= $(call my-dir)
 
 UFO_PROJECT_PATH := $(LOCAL_PATH)
 
+# ------------------------------------------------------------------------
+## include ufo_headers.mk for needed header exports
+include $(LOCAL_PATH)/ufo_headers.mk
+# ------------------------------------------------------------------------
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := ufo
@@ -84,15 +89,6 @@ OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
 # Do not strip, as debug section creation will fail because already stripped.
 LOCAL_STRIP_MODULE := false
 
-# Use of LOCAL_COPY_HEADERS is deprecated.
-
-LOCAL_COPY_HEADERS_TO := libpavp/
-
-LOCAL_COPY_HEADERS :=
-LOCAL_COPY_HEADERS += inc/libpavp/libpavp.h
-# How it should have been done?
-## LOCAL_COPY_HEADERS += include/libpavp/libpavp.h
-
 include $(BUILD_SYSTEM)/dynamic_binary.mk
 
 # Inclusion of all_copied_headers invokes code for LOCAL_COPY_HEADERS.
@@ -101,29 +97,5 @@ $(linked_module): $(UFO_PROJECT_PATH)/dist/system/lib/libpavp.so.xz all_copied_h
 	mkdir -p $(dir $@)
 	xz -d -c $< >$@
 	chmod --reference=$< $@
-
-# ------------------------------------------------------------------------
-
-include $(CLEAR_VARS)
-
-# Use of LOCAL_COPY_HEADERS is deprecated.
-
-# Inclusion of all_copied_headers invokes code for LOCAL_COPY_HEADERS.
-# This "module" does not have a dependency for which all_copied_headers
-# can be made a prerequisite, so depend on the reference above for
-# proper execution with "mm" command.
-
-LOCAL_COPY_HEADERS_TO := ufo/
-
-LOCAL_COPY_HEADERS :=
-LOCAL_COPY_HEADERS += inc/ufo/graphics.h
-LOCAL_COPY_HEADERS += inc/ufo/gralloc.h
-# How it should have been done?
-## LOCAL_COPY_HEADERS += include/ufo/graphics.h
-## LOCAL_COPY_HEADERS += include/ufo/gralloc.h
-
-include $(BUILD_COPY_HEADERS)
-
-# ------------------------------------------------------------------------
 
 endif # ifneq ($(BOARD_HAVE_GEN_GFX_SRC),true)
